d2a3cd6ebaaab2eafa736ef3ccde22c1f264ed95
Annabel Edwards <annabeledwards@Annabels-MacBook-Pro-2.local>
2024-01-16 23:35:42 -0600

    added border to laanding page

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -32,7 +32,7 @@ const Container = styled.div`
   min-height: 750px;
   margin-top: 0px;
   position: relative;
-  background: var(--darkgreen);
+  background: #003800;
   color: white;
   display: flex;
   flex-direction: column;
@@ -55,6 +55,26 @@ const Title = styled.h1`
 
   ${mobile} {
     font-size: 50px; /* Apply mobile styles directly to Title */
+  }
+`;
+
+const Boundary = styled.div<{ top: boolean }>`
+  width: 100%;
+  overflow: hidden;
+  background-color: #003800;
+  line-height: 0;
+  transform: ${(props) => props.top && 'rotate(180deg)'};
+
+  svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 107px;
+    transform: rotateY(180deg);
+  }
+
+  .shape-fill {
+    fill: var(--darkgreen);
   }
 `;
 
@@ -138,6 +158,20 @@ const IvyIcon = styled(FaLeaf)<IvyIconProps>`
 
 export const Landing: React.FC = () => {
   return (
+    <>
+    <Boundary top>
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </Boundary>
     <Container id="landing">
       <BookContainer topPosition={70} rightPosition={0} className="hide">
         <Bookshelf>
@@ -194,6 +228,20 @@ export const Landing: React.FC = () => {
 
       <Bookshelf></Bookshelf>
     </Container>
+    <Boundary top={false}>
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </Boundary>
+    </>
   );
 };
 
