6e7fb464bd961f6bcc0ecc08b5d529b0547f32e3
Dilan <dilan@dilanxd.com>
2024-01-31 20:41:32 -0600

    update linkedin url

--- src/variables.ts
+++ src/variables.ts
@@ -3,7 +3,6 @@
 export const dashboard = 'https://dashboard.wildhacks.net/';
 
 export const instagram = 'https://www.instagram.com/wildhacks/';
-export const linkedin =
-  'https://www.linkedin.com/company/northwestern-wildhacks/';
+export const linkedin = 'https://linkedin.com/company/wildhacks/';
 
 export const email = 'wildhacks@northwestern.edu';
